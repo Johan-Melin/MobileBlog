91aefbd6ab53ca49ade87c48c26bbde34216837a
Johan Melin <johan.melin@mail.com>
2020-03-17 17:39:03 +0100

    added json-server

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://60fb3a54.ngrok.io/'
+});
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,11 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons'
 //import renaming: Context as BlogContext
 
 const IndexScreen = ({ navigation }) => {
-  const { state, deleteBlogPost } = useContext(Context);
+  const { state, deleteBlogPost, getBlogPosts } = useContext(Context);
+
+  //the empty array makes sure it's only run once
+  useEffect(() => {
+    getBlogPosts();
+
+    const listener = navigation.addListener('didFocus', () => {
+      getBlogPosts();
+    });
+
+    // clean addListener if unmount, this only runs if stopped
+    return () => {
+      listener.remove();
+    }
+  }, []);
 
   return (
     <View style={styles.container}>
